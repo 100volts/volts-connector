2d9ccb50d4de4dded7c79719e3c9bfd5a550ede6
irumenov <ivaylo.rumenov2@dxc.com>
2025-09-12 09:35:41 +0300

    Added a implementaion for the waithing when reading using setInterval

--- debian/src/DisplaymeterData.ts
+++ debian/src/DisplaymeterData.ts
@@ -1,27 +1,3 @@
-#!/usr/bin/env node
-
-import { login } from "./login";
-import { loadConfig } from "./config/LoadConfigData";
-import { ConfigData } from "./domain/ConfigData";
-import ReadMeters from "./modbus/ReadMeters";
-import postMeterData from "./SendMeterData";
-//import chalk from "chalk";
-
-async function app() {
-  console.log("Hello, app is running");
-  try {
-    const configData: ConfigData = await loadConfig();
-    console.log("Config data: ", configData);
-    const token = await login(configData.hostname); // wait for login and get the token
-    console.log("Token received in index.ts:", token);
-    //console.log("Read data:", ReadMeters())
-    postMeterData(await ReadMeters(), "localhost", token);
-    console.log("data sent");
-  } catch (err) {
-    console.error("Login failed:", err);
-  }
-}
-
 async function app2() {
   console.log("Hello, app is running");
   try {
@@ -107,5 +83,3 @@ async function app2() {
     console.error("Login failed:", err);
   }
 }
-
-app();
